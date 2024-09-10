6caf0dc7c924897b2de04cbc765c034e257fadc5
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-09-10 14:10:35 +0530

    handling url

--- Project-2/index.js
+++ Project-2/index.js
@@ -1,20 +1,38 @@
-const  http=require('http');
-const fs =require('fs');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-const server=http.createServer((req,res)=>{
-    if (req.url === "/favicon.ico") return res.end();
-    fs.appendFile("data.txt",`url is ${req.url} \n`,(error,data)=>
-        {
+const server = http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") return res.end(); // ignore favicon requests
 
-        switch(req.url){
-            case '/': res.end("This is home page");
-            break;
-            case '/contect-us': res.end("This is contect page");
-            break;
-            case '/project': res.end("This is project page");
-            break;
-            default: res.end("Enter proper url")
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+
+    fs.appendFile("data.txt", `url is ${req.url} \n`, (error) => {
+        if (error) {
+            res.end("Error writing to file");
+            return;
         }
-    })
+
+        switch (myUrl.pathname) {
+            case '/':
+                res.end("This is home page");
+                break;
+            case '/contect-us':
+                res.end("This is contact page");
+                break;
+            case '/search':
+                const search = myUrl.query.searchitem;
+                res.end(`Your searched item is ${search}`);
+                break;
+            case '/project':
+                const name = myUrl.query.name;
+                res.end(`Hi, ${name}`);
+                break;
+            default:
+                res.end("Enter proper url");
+        }
+    });
 });
-server.listen(8000,()=>console.log("Successful conntect"))
+
+server.listen(8000, () => console.log("Server is running on port 8000"));
